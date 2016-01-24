0865c32af3a42e96041a7da789c8d61f9ff54af7
hdznrrd <github@follvalsch.de>
2016-01-24 19:53:31 +0100

    allow reboot

--- qemu/startqemu.sh
+++ qemu/startqemu.sh
@@ -10,4 +10,4 @@ if [[ -z $2 ]]; then
     exit 1
 fi
 
-qemu-system-arm -kernel $1 -cpu arm1176 -m 256 -M versatilepb -no-reboot -serial stdio -append "root=/dev/sda2 panic=1" -hda $2 -redir tcp:5022::22
+qemu-system-arm -kernel $1 -cpu arm1176 -m 256 -M versatilepb -serial stdio -append "root=/dev/sda2 panic=1" -hda $2 -redir tcp:5022::22
